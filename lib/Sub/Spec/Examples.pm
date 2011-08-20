2056fa0fe8fc2484ee5cfaf954a487e0e079bf63
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-20 14:42:45 +0700

    add function: randlog

--- lib/Sub/Spec/Examples.pm
+++ lib/Sub/Spec/Examples.pm
@@ -4,10 +4,13 @@ use 5.010;
 use strict;
 use warnings;
 
+use List::Util qw(min max);
+use Log::Any '$log';
+
 # VERSION
 
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(delay dies err);
+our @EXPORT_OK = qw(delay dies err randlog);
 our %SPEC;
 
 $SPEC{delay} = {
@@ -74,6 +77,55 @@ sub err {
     my $code = int($args{code}) // 0;
     $code = 500 if $code < 100 || $code > 555;
     [$code, "Response $code"];
+}
+
+my %num_levels = (
+    fatal => 1, error => 2, warn  => 3,
+    info  => 4, debug => 5, trace => 6,
+);
+my %str_levels = reverse %num_levels;
+#use Data::Dump; dd(\%num_levels); dd(\%str_levels);
+$SPEC{randlog} = {
+    summary => "Produce some random Log::Any log messages",
+    description => <<'_',
+
+_
+    args => {
+        n => ['int*' => {
+            summary => 'Number of log messages to produce',
+            arg_pos => 0,
+            default => 10,
+        }],
+        min_level => ['str*' => {
+            summary => 'Minimum level',
+            default => 'fatal',
+            in      => [keys %num_levels],
+        }],
+        max_level => ['str*' => {
+            summary => 'Maximum level',
+            default => 'trace',
+            in      => [keys %num_levels],
+        }],
+    },
+};
+sub randlog {
+    my %args      = @_;
+    my $n         = $args{n} // 10;
+    my $min_level = $args{min_level} // "fatal";
+    $min_level    = $str_levels{ min(keys %str_levels) }
+        if !defined($num_levels{$min_level});
+    my $max_level = $args{max_level} // "trace";
+    $max_level    = $str_levels{ max(keys %str_levels) }
+        if !defined($num_levels{$max_level});
+
+    for my $i (1..$n) {
+        my $num_level = int($num_levels{$min_level} +
+            rand()*($num_levels{$max_level}-$num_levels{$min_level}+1));
+        my $str_level = $str_levels{$num_level};
+        $log->$str_level("This is random log message #$i, level=$str_level: ".
+                             int(rand()*9000+1000));
+    }
+    [200, "OK"];
 }
 
 1;
