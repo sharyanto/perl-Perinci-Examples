a9e76eacfea700243ed13c49386e6942dee20314
perlancar (@pc-home) <perlancar@gmail.com>
2015-01-11 18:03:44 +0700

    [doc] Give explanation for functions in Perinci::Examples::FileStream

--- lib/Perinci/Examples/FileStream.pm
+++ lib/Perinci/Examples/FileStream.pm
@@ -16,7 +16,7 @@ $SPEC{':package'} = {
     summary => 'Examples for reading/writing files (using streaming result)',
     description => <<'_',
 
-The functions in this package demo streaming input and output.
+The functions in this package demonstrate byte streaming of input and output.
 
 The functions are separated into this module because these functions read/write
 files on the filesystem and might potentially be dangerous if
@@ -34,6 +34,22 @@ $SPEC{read_file} = {
         path => {schema=>'str*', req=>1, pos=>0},
     },
     result => {schema=>'buf*', stream=>1},
+    description => <<'_',
+
+This function demonstrate output streaming of bytes.
+
+To do output streaming, on the function side, you just return an opened
+filehandle glob which can be read from, or an `IO::Handle` object (anything that
+supports `getline()` or `getitem()` method) or an array (usually a tied array).
+
+`Perinci::CmdLine` supports this and will read items/lines from the
+handle/object/tied array until exhausted, and then output them.
+
+This works over remote (HTTP) too, because output streaming is supported by
+`Riap::HTTP` (version 1.2) and `Perinci::Access::HTTP::Client`. Streams are
+translated into HTTP chunks.
+
+_
 };
 sub read_file {
     my %args = @_; # VALIDATE_ARGS
@@ -50,7 +66,23 @@ $SPEC{write_file} = {
     v => 1.1,
     description => <<'_',
 
-Overwrites existing file.
+This function demonstrates input streaming of bytes.
+
+To do input streaming, on the function side, you just specify one your args with
+the `stream` property set to true (`stream => 1`). In this example, the
+`content` argument is set to streaming.
+
+If you run the function through `Perinci::CmdLine`, you'll get a filehandle
+instead of the actual value. You can then iterate the content from this
+filehandle. This currently works for local functions only. As of this writing,
+`Riap::HTTP` protocol does not support input streaming. It supports partial
+input though (see the documentation on how this works) and theoretically
+streaming can be emulated by client library using partial input. However, client
+like `Perinci::Access::HTTP::Client` does not yet support this.
+
+Note that the argument's schema is still `buf*`, not `obj*`.
+
+Note: This function overwrites existing file.
 
 _
     args => {
@@ -83,6 +115,9 @@ sub write_file {
 $SPEC{append_file} = {
     v => 1.1,
     description => <<'_',
+
+This is like `write_file()` except that it appends instead of overwrites
+existing file.
 
 _
     args => {
