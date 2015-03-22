2f21ca07ced8ca4afd88d51eb7bf87f34886df41
perlancar (@pc-home) <perlancar@gmail.com>
2015-03-22 20:41:35 +0700

    [doc] Some rewording of description, remove Synopsis

--- lib/Perinci/Examples.pm
+++ lib/Perinci/Examples.pm
@@ -1016,22 +1016,16 @@ sub multi_status {
 1;
 # ABSTRACT:
 
-=head1 SYNOPSIS
-
- use Perinci::Examples qw(delay);
- delay();
-
-
 =head1 DESCRIPTION
 
-This module and its submodules contain an odd mix of various functions,
-variables, and other code entities, along with their L<Rinci> metadata. Mostly
-used for testing Rinci specification and the various L<Perinci> modules.
+This distribution contains an odd mix of various functions, variables, and other
+code entities, along with their L<Rinci> metadata. Mostly used for testing Rinci
+specification and the various L<Perinci> modules.
 
 Example scripts are put in a separate distribution (see
 L<Perinci::Examples::Bin>) to make dependencies for this distribution minimal
-(e.g. not depending on L<Perinci::CmdLine>) since this example module(s) are
-usually used in the tests of other modules.
+(e.g. not depending on L<Perinci::CmdLine::Any>) since this example module(s)
+are usually used in the tests of other modules.
 
 
 =head1 SEE ALSO
