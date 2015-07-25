4178396c4ccf00208c2cfd0c61fed42012e1c08e
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-07-25 13:55:23 +0700

    Add module: Perinci::Examples::Version to demonstrate Riap package metadata which contains entity_v that is different from package $VERSION

---
+++ lib/Perinci/Examples/Version.pm
@@ -0,0 +1,16 @@
+package Perinci::Examples::Version;
+
+# DATE
+# VERSION
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Package that contains entity_v in its Rinci package metadata',
+    entity_v => 9.99,
+};
+
+# ABSTRACT:
+
+=head1 DESCRIPTION
