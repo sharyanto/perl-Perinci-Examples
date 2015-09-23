71343e6fb2c44980121c98ac02055e0f8ea81b4d
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-23 10:01:34 +0700

    Add module: Perinci::Examples::Tiny::Args

---
+++ lib/Perinci/Examples/Tiny/Args.pm
@@ -0,0 +1,40 @@
+package Perinci::Examples::Tiny::Args;
+
+# DATE
+# VERSION
+
+our %SPEC;
+
+# this Rinci metadata is already normalized
+$SPEC{has_dot_args} = {
+    v => 1.1,
+    summary => "This function contains arguments with dot in their names",
+    args => {
+        'a.number' => {
+            schema => ['int' => {req=>1}, {}],
+            pos => 0,
+            req => 1,
+        },
+        'another.number' => {
+            schema => ['float' => {req=>1}, {}],
+            pos => 1,
+            req => 1,
+        },
+    },
+    result => {
+        summary => 'Return the two numbers multiplied',
+    },
+};
+sub has_dot_args {
+    my %args = @_;
+    [200, "OK", $args{'a.number'} * $args{'another.number'}];
+}
+
+1;
+# ABSTRACT: Tests related to function arguments
+
+=head1 DESCRIPTION
+
+Like th other Perinci::Examples::Tiny::*, this module does not use other modules
+and is suitable for testing Perinci::CmdLine::Inline as well as other
+Perinci::CmdLine frameworks.
